698ba978a3ca8ff28cab3fdf6cdc0a3aa71d260f
benxin niu <bn2645@mun.ca>
2018-06-29 22:04:34 -0400

    added test information

---
+++ src/smartcontract/contractInfo.js
@@ -0,0 +1,4 @@
+const address = "n1fZprnBvDhixrLExkHmC9cUNSi27S94g6z";
+const register_scavenger_req = ["{\"name\":\"benxin\"}"];
+const create_new_puzzel = ["{\"latitude\":\"1232133221\", \"longitude\":\"3214412131\", \"reward\":30, \"active\":true, \"answer\":\"aa\"}"];
+const submit_answer = ["{\"id\":182, \"travel\":\"teleport\", \"answer\":\"aa\"}"];
